fcfbf7455c936cf3acc6efdbd0b5f305bea144fc
Livia Schoen <liviabellaschoen@gmail.com>
2017-01-24 20:08:31 -0500

    Integration Livia et Papa

--- MMUITurtle.py
+++ MMUITurtle.py
@@ -16,11 +16,37 @@ class MMUITurtle:
                                 ((-34, 27),   (2, 27),    (37, 27),   (72, 27)),
                                 ((-34, 67),   (2, 67),    (37, 67),   (72, 67)),
                                 ((-34, 107),  (2, 107),   (37, 107),  (72, 107))]
+        self.pinResultTable =  [((-83, -173), (-69, -173), (-83, -159), (-69, -159)),
+                                ((-82, -133), (-68, -133), (-83, -120), (-69, -120)),
+                                ((-83, -94),  (-69, -94),  (-83, -80),  (-68, -80)),
+                                ((-82, -53),  (-68, -53),  (-82, -40),  (-69, -40)),
+                                ((-83, -14),  (-68, -14),  (-83, -0),   (-68, -0)),
+                                ((-83, 26),   (-68, 26),   (-83, 40),   (-68, 40)),
+                                ((-83, 66),   (-68, 66),   (-83, 80),   (-68, 80)),
+                                ((-83, 106),  (-68, 106),  (-83, 120),  (-68, 120))]
+
+
+    def circleclick(self, poso, post, color):
+        self.tt.up()
+        self.tt.setpos(poso, post)
+        self.tt.down()
+        self.tt.color("black", color)
+        self.tt.begin_fill()
+        self.tt.circle(20)
+        self.tt.end_fill()
 
     def display(self):
         self.tt.hideturtle()
         self.screen.bgpic("./data/mm_board.gif")
         self.screen.onclick(self.onclick)
+        self.circleclick(-163, -303, "pink")
+        self.circleclick(-113, -303, "white")
+        self.circleclick(-63, -303, "red")
+        self.circleclick(-13, -303, "yellow")
+        self.circleclick(43, -303, "orange")
+        self.circleclick(93, -303, "green")
+        self.circleclick(143, -303, "blue")
+        self.circleclick(193, -303, "gray")
         self.screen.mainloop()
 
     def drawcircle(self, pos, color, size):
@@ -32,16 +58,35 @@ class MMUITurtle:
         self.tt.circle(size)
         self.tt.end_fill()
 
+    def getcolor(self, x, y):
+        print("getcolor: ", x ,y)
+        if x > -183 and x < -143 and y > -303 and y < -263:
+            return "pink"
+        elif x > -133 and x < -93 and y > -303 and y < -263:
+            return "white"
+        elif x > -83 and x < -43 and y > -303 and y < -263:
+            return "red"
+        elif x > -33 and x < 6 and y > -303 and y < -263:
+            return "yellow"
+        elif x > 23 and x < 63 and y > -303 and y < -263:
+            return "orange"
+        elif x > 73 and x < 113 and y > -303 and y < -263:
+            return "green"
+        elif x > 123 and x < 163 and y > -303 and y < -263:
+            return "blue"
+        elif x > 173 and x < 213 and y > -303 and y < -263:
+            return "gray"
+        return "no color"
+
     def onclick(self, x, y):
-        print("MMUITurtle: ", x, y)
+        color = self.getcolor(x, y)
         line = self.board.getcurrentrow()
         print("Position on board: ", line, len(self.currentLine))
 
-        color = "green"
         self.drawcircle(self.pincolorTable[line][len(self.currentLine)], color, self.pincolorSize)
         self.currentLine.append(color)
         if len(self.currentLine) == 4:
-            self.board.addRow(color)
+            self.board.addRow(self.currentLine)
             self.currentLine = []
 
     def setboard(self, board):
